01e01f282d2dc23de04bd9d69cd88c09809853b9
Bryan Muller <mul13001@byui.edu>
2018-06-18 22:00:07 -0600

    finish lab 7 start lab9

--- lab6/lab06.cpp
+++ lab6/lab06.cpp
@@ -1,28 +1,41 @@
 /***********************************************************************
 * Program:
 *    Lab 06, Vulnerabilities and Exploits
-*    Brother Wilson,, CS470
+*    Brother Wilson, CS470
 * Author:
 *    Bryan Muller
 * Summary: 
 *    This program is designed to demonstrate memory injection vulnerabilities.
-*
+*    Note: StyleChecker is saying that I am missing proper indentation
+ *         but in my source file, everything is indented correctly.
+ *         I'm not sure what's up with that.
 ************************************************************************/
 
 #include <iostream>
 #include <cassert>
 #include <string>
+
+
 using namespace std;
 
 void stackExploit();
+
 void heapExploit();
+
 void arrayExploit();
+
 void intExploit();
+
 void arcExploit();
+
 void subterfugeExploit();
+
 void vtableExploit();
+
 void ansiExploit();
+
 void stackSmasher();
+
 /******************************************************
  * MAIN
  * This function take you to the various exploits
@@ -87,16 +100,16 @@ int main()
  * 2. There must be a way to overwrite the pointer
  * 3. After the pointer is dereferenced, the pointer is dereferenced
  ****************************************/
-void subterfugeVulnerability(long * array, int size)
+void subterfugeVulnerability(long *array, int size)
 {
    long int buffer[2] = {0, 1};
-   const char * message = "All is safe; nothing bad happened\n";  // 1. Pointer
+   const char *message = "All is safe; nothing bad happened\n";  // 1. Pointer
 
    for (int i = 0; i < size; i++)  // 2. Overwrite
       buffer[i] = array[i];
 
    cout << message;                // 3. Dereference
-   
+
 }
 
 /****************************************
@@ -110,15 +123,15 @@ void subterfugeExploit()
 {
    // an attacker's array
    long int array[3] =             // 1. Exploit
-   {
-      0,
-      1,
-      (long int)"!!!!!!!!!!!!! You've been hacked !!!!!!!!!!!!!\n"
-      // 2. Provide pointer
-   };
+      {
+         0,
+         1,
+         (long int) "!!!!!!!!!!!!! You've been hacked !!!!!!!!!!!!!\n"
+         // 2. Provide pointer
+      };
 
    // exploit it
-   subterfugeVulnerability(array, 3);   
+   subterfugeVulnerability(array, 3);
 }
 
 /**************************************************************
@@ -155,10 +168,7 @@ void stackVulnerability(long int userInput[], long int size)
 /*********************************************
  * STACK EXPLOIT
  * 1. The attacker must provide more data into the
- *    outwardly facing buffer than the buffer   int buffer[12] = {0, 1, 2, 3, 4, 5, 6, (long int) &displayMessage};
-
-   stackVulnerability(buffer, sizeof(buffer)/sizeof(buffer[0]));
-}is designed to hold
+ *    outwardly facing buffer than the buffer is designed to hold
  * 2. The attacker must know where the the stack pointer resides
  *    on the stack. This should be just beyond the end of the buffer
  * 3. The attacker must insert machine language instructions in the buffer.
@@ -174,12 +184,16 @@ void stackExploit()
    // create a buffer longer than it should be.
    // Throw a function pointer on the end so we can
    // have some fun.
-   long int buffer[10] = { 0, 1, 2, 3, 4, (long int) &stackSmasher};
-   int size = sizeof(buffer)/sizeof(buffer[0]);
+   long int buffer[10] = {0, 1, 2, 3, 4, (long int) &stackSmasher};
+   int size = sizeof(buffer) / sizeof(buffer[0]);
 
-   cout << "Starting exploit" << endl;
+   cout << "Demonstrating exploit by inserting a function address into the "
+        << "call stack" << endl;
+   cout << "This demonstrates how a dangerous function call could be "
+        << "introduced into the stack" << endl;
+   cout << "Beginning stack smash" << endl;
    stackVulnerability(buffer, size);
-   cout << "Exploit completed";
+   cout << "Stack smashed" << endl;
 }
 
 
@@ -206,8 +220,22 @@ void stackSmasher()
  *    not correctly check for the buffer size.
  * 4. The second buffer must be released before the first.
  *************************************/
-void heapVulnerability(/* feel free to add parameters */)
+void heapVulnerability(long int *userInput, int size)
 {
+   // Create two buffers on the Heap
+   long int *vulBuffer1 = new long[2];
+   long int *vulBuffer2 = new long[2];
+
+   // overwrite the first buffer
+   // without checking bounds
+   for (int i = 0; i < size; i++)
+   {
+      vulBuffer1[i] = userInput[i];
+   }
+
+   // remove the second before the first
+   delete[] vulBuffer2;
+   delete[] vulBuffer1;
 }
 
 /*************************************
@@ -221,6 +249,12 @@ void heapVulnerability(/* feel free to add parameters */)
  ***********************************/
 void heapExploit()
 {
+   // use this buffer to spray the heap
+   // with new values
+   long int buffer[6] = {-5, -5, -5, -5, -5, -5};
+
+   cout << "We will now employ heap spraying to crash the program" << endl;
+   heapVulnerability(buffer, sizeof(buffer) / sizeof(buffer[0]));
 }
 
 /**************************************************************
@@ -235,8 +269,16 @@ void heapExploit()
  * 2. The array index variable must be reachable through external input.
  * 3. There must not be bounds checking on the array index variable.
  ************************************/
-void arrayVulnerability(/* feel free to add parameters */)  
+void arrayVulnerability(int i, int userInput)
 {
+   // initialize an array
+   int vulnerableArray[2] = {1, 2};
+   // For debugging
+   assert(vulnerableArray[i] == 0);
+   // set the ith value of the array to the
+   // provided value with no bounds checking
+   cout << "Putting in address " << hex << &vulnerableArray[i] << endl;
+   vulnerableArray[i] = userInput;
 }
 
 /**************************************
@@ -249,6 +291,29 @@ void arrayVulnerability(/* feel free to add parameters */)
  *************************************/
 void arrayExploit()
 {
+   // Create a variable
+   int meaningOfLife = 0;
+   cout << "We will demonstrate an array index exploit by changing the value"
+        << " of meaningOfLife" << endl;
+   cout << "Intial value for meaningOfLife: " << meaningOfLife << endl;
+   cout << "Meaning of life address: " << hex << &meaningOfLife << endl;
+
+   /**
+    * Pass an array index to access and a new value.
+    * This exploit should use the unchecked array bounds
+    * in the vulnerability to change the value of meaningOfLife
+    * to 42.
+    *
+    * NOTE: I was getting this to work on the linux lab computer,
+    * but not on my local machine as the compiler is different.
+    * Depending on which machine the program is compiled on, you
+    * may need to provide a different index to the vulnerability
+    * to get it to change meaningOfLife.
+    */
+   cout << "Use the vulnerability to change the value of meaningOfLife"
+        << endl;
+   arrayVulnerability(15, 42);
+   cout << "The meaning of life is " << dec << meaningOfLife << endl;
 }
 
 
@@ -265,8 +330,18 @@ void arrayExploit()
  * 3. At least one of the numbers used to compute the sentinel
  *    must be reachable through external input.
  *********************************************/
-void intVulnerability(/* feel free to add parameters */)
+void intVulnerability(char exploitBuffer[], unsigned char size,
+                      int index, int exploitVal)
 {
+   if ((unsigned char) index <= size && index >= 0)
+   {
+      cout << "Putting in address " << hex << &exploitBuffer[index] << endl;
+      exploitBuffer[index] = exploitVal;
+   }
+   else
+   {
+      cout << dec << (unsigned char) index << endl;
+   }
 }
 
 /**********************************************
@@ -278,6 +353,35 @@ void intVulnerability(/* feel free to add parameters */)
  **********************************************/
 void intExploit()
 {
+   // Create a new buffer
+   // Using characters since they have a small size
+   char buffer[256];
+   // This is the variable we will change using the
+   // exploit
+   int theMeaningOfLife = 0;
+   // for finding the correct address
+   cout << "Again we will use an integer overflow exploit to change"
+        << " the value of theMeaningOfLife" << endl;
+   cout << "Initial value: " << dec << theMeaningOfLife << endl;
+   cout << "Meaning of life address: " << hex
+        << &theMeaningOfLife << endl;
+
+
+   /**
+    * This is tricky. When the 268 is cast to an
+    * unsigned char for the comparison, it overflows
+    * resulting in a zero. This allows us to bypass the
+    * size check comparison as index is still > 0 when
+    * not cast. This allows us to change the value in
+    * theMeaningOfLife. Again, depending on which computer
+    * you compile and run this on, you will need to change
+    * the value of 268.
+    */
+   cout << "Invoke the exploit" << endl;
+   intVulnerability(buffer, 255, 268, 42);
+
+   cout << "The meaning of life is: " << dec << theMeaningOfLife << endl;
+
 }
 
 /**************************************************************
@@ -285,6 +389,24 @@ void intExploit()
  ********************      ARC       **************************
  **************************************************************
  **************************************************************/
+
+/**
+ * The function that is supposed to be executed
+ * by the ARC Vulnerability function
+ */
+void theMeaningOfLife()
+{
+   cout << "The Meaning of Life is 42" << endl;
+}
+
+/**
+ * The function we will call using
+ * the ARC exploit function
+ */
+void compromisedMeaningOfLife()
+{
+   cout << "Life is meaningless" << endl;
+}
 
 /*********************************
  * ARC VULNERABILITY
@@ -295,8 +417,23 @@ void intExploit()
  * 3. After the memory is overwritten, the function pointer must
  *    be dereferenced
  ********************************/
-void arcVulnerability(/* feel free to add parameters */)
+void arcVulnerability(long *userInput, int size)
 {
+   // Create a buffer to copy user input into
+   long uncheckedBuffer[2];
+   // Here is our function pointer, right
+   // after the unchecked buffer in memory
+   void (*pntFunc)() = theMeaningOfLife;
+
+   for (int i = 0; i < size; i++)
+   {
+      // copy all of userInput into the
+      // vulnerable buffer without checking size
+      uncheckedBuffer[i] = userInput[i];
+   }
+
+   // Call our function pointer to 'theMeaningOfLife'
+   pntFunc();
 }
 
 /*********************************
@@ -308,6 +445,14 @@ void arcVulnerability(/* feel free to add parameters */)
   ********************************/
 void arcExploit()
 {
+   // Create a payload with a pointer to a new function
+   long payload[3] = {0, 0, (long) compromisedMeaningOfLife};
+   cout << "The arcVulnerability function should call the "
+        << "meaningOfLife function to display its message"
+        << ". \nUsing ARC injection, we will overwrite the "
+        << "function pointer and inject a compromised function "
+        << "in its place" << endl;
+   arcVulnerability(payload, 3);
 }
 
 /**************************************************************
@@ -327,6 +472,42 @@ void arcExploit()
  **********************************/
 class Vulnerability
 {
+public:
+   // the buffer we will overflow
+   long buffer[2];
+
+   /* our function that sets the buffer with no bounds
+   * check
+   */
+   void setBuffer(long newVal, int i)
+   {
+      buffer[i] = newVal;
+   }
+
+   /* our virtual function to be
+   // implmented in our child class
+    */
+   virtual void getBuffer() = 0;
+};
+
+/**
+ * Child class that inherits some dangerous
+ * code from Vulnerability
+ */
+class Dangerous : public Vulnerability
+{
+
+public:
+
+   /*
+    * implement the getBuffer virtual function
+    * to display the contents of buffer
+    */
+   virtual void getBuffer()
+   {
+      cout << "Buffer contents " << buffer[0]
+           << " " << buffer[1] << endl;
+   }
 };
 
 /************************************
@@ -338,6 +519,21 @@ class Vulnerability
  ***********************************/
 void vtableExploit()
 {
+   // new class instance
+   Dangerous exploit;
+   // our function pointer
+   void (*pntFun)() = compromisedMeaningOfLife;
+
+   cout << "Demonstrates VTABLE exploit by overwriting"
+        << " the pointer to the getBuffer method on the vtable"
+        << " and inserting a pointer to our compromised function instead.\n"
+        << " Instead of printing out the contents of the buffer, it will"
+        << " execute our compromised function" << endl;
+   // use the setBuffer to overflow the buffer
+   // and overwrite the function pointer on the vtable
+   exploit.setBuffer((long) pntFun, -2);
+   // call the getBuffer method
+   exploit.getBuffer();
 }
 
 
@@ -353,8 +549,20 @@ void vtableExploit()
  * 2. Validation of the buffer must check the size of the buffer
  *    rather than the number of elements in the buffer.
  ********************************************************/
-void ansiVulnerability(/* feel free to add parameters */)
+short *ansiVulnerability(const char *inputText, int size)
 {
+   // allocate a new array on the heap
+   short *toCopy = new short;
+
+   for (int i = 0; i < size; i++)
+   {
+      // copy the string into the short
+      toCopy[i] = (short) inputText[i];
+   }
+
+   // return a pointer to the newly allocated
+   // unicode value
+   return toCopy;
 }
 
 /***********************************************
@@ -364,4 +572,10 @@ void ansiVulnerability(/* feel free to add parameters */)
  **********************************************/
 void ansiExploit()
 {
+   /**
+    * I'm not sure if this is what we were supposed to do,
+    * but this will spray the heap and crash the program
+    */
+   short *value = ansiVulnerability("abcdefghijklmnopqrstuvwxyz", 26);
+   delete value;
 }
---
+++ lab9/template.cpp
@@ -0,0 +1,14 @@
+/***********************************************************************
+* Program:
+*    Lab xx, name
+*    Brother Helfrich, CS470
+* Author:
+*    your name
+* Summary: 
+*    Please include a detailed description of:
+*    1. What this program is designed to do
+*    2. How this program will go about solving the problem
+*    3. What assumptions about input (file or user) is made
+*    4. Citations if the algorithm was borrowed or inspired from any source
+************************************************************************/
+
